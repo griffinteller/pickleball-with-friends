9ff188055cd85fc3c4dcc5f7cff7ba3b58d10b27
Griffin Teller <griffinteller@gmail.com>
2021-02-12 23:48:20 -0800

    Got syncing quaternion between phone and main app

--- PWF Remote/Assets/Scripts/Player/GyroscopeSocketClient.cs
+++ PWF Remote/Assets/Scripts/Player/GyroscopeSocketClient.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using WebSocketSharp;
 
@@ -5,6 +6,8 @@ namespace Player
 {
     public class GyroscopeSocketClient : MonoBehaviour
     {
+        public Gyroscope gyroscope;
+        
         private WebSocket _socket;
         private ConnectionStatus _connectionStatus;
         
@@ -15,8 +18,41 @@ namespace Player
 
             _socket = new WebSocket("ws://10.0.0.37/pwfsocket");
             _socket.Connect();
-            _socket.Send("Hello!");
+            _connectionStatus = ConnectionStatus.Connected;
         }
+
+        public void Update()
+        {
+            if (_connectionStatus != ConnectionStatus.Connected)
+                return;
+
+            try
+            {
+                _socket.Send(QuaternionToBytes(gyroscope.Attitude));
+            }
+            catch (InvalidOperationException)
+            {
+                _connectionStatus = ConnectionStatus.Disconnected;
+            }
+        }
+
+        private static byte[] QuaternionToBytes(Quaternion q) // 2 bytes per float, big endian
+        {
+            byte[] x = BitConverter.GetBytes(q.x);
+            byte[] y = BitConverter.GetBytes(q.y);
+            byte[] z = BitConverter.GetBytes(q.z);
+            byte[] w = BitConverter.GetBytes(q.w);
+
+            byte[] result = new byte[16];
+            x.CopyTo(result, 0);
+            y.CopyTo(result, 4);
+            z.CopyTo(result, 8);
+            w.CopyTo(result, 12);
+
+            return result;
+        }
+        
+        
     }
 
     public enum ConnectionStatus
--- PWF Remote/Assets/Scripts/Player/GyroscopeText.cs
+++ PWF Remote/Assets/Scripts/Player/GyroscopeText.cs
@@ -1,49 +1,25 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.Mathematics;
 using UnityEngine;
+using Gyroscope = Player.Gyroscope;
 
 public class GyroscopeText : MonoBehaviour
 {
-    private TMP_Text _text;
-    private Quaternion _rawAttitude = Quaternion.identity;
-    private Quaternion _zeroAttitude = Quaternion.identity;
+    public Gyroscope gyroscope;
     
-    public Quaternion Attitude => Quaternion.Inverse(_zeroAttitude) * _rawAttitude;
+    private TMP_Text _text;
 
-    public void Start()
+    public void Awake()
     {
         _text = GetComponent<TMP_Text>();
-        Input.gyro.enabled = true;
-        GetRawAttitude();
-        Calibrate();
     }
     
     public void Update()
     {
-        GetRawAttitude();
         SetText();
-    }
-
-    public void Calibrate()
-    {
-        _zeroAttitude = _rawAttitude;
-    }
-
-    private void GetRawAttitude()
-    {
-        _rawAttitude = GyroToUnity(Input.gyro.attitude);
     }
 
     private void SetText()
     {
-        _text.text = Attitude.eulerAngles.ToString();
-    }
-
-    private static Quaternion GyroToUnity(Quaternion q)
-    {
-        return new Quaternion(q.x, q.y, -q.z, -q.w);
+        _text.text = gyroscope.Attitude.eulerAngles.ToString();
     }
 }
